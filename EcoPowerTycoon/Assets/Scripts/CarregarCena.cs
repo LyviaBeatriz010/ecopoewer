f6c74570f7e9aa4fe90c02659f9c44b8e3f06c72
LyviaBeatriz010 <lyvia.v@escolar.ifrn.edu.br>
2024-06-26 19:00:01 -0300

    Paineis de vitória e derrota e menu inicial feitos

---
+++ EcoPowerTycoon/Assets/Scripts/CarregarCena.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarregarCena : MonoBehaviour
+{
+   public void CarregarJogo()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
