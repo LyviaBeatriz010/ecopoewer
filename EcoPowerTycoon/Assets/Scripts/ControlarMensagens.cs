c67018ddf09e7749a6fd04a73e7c9ca2b4f784d1
LyviaBeatriz010 <lyvia.v@escolar.ifrn.edu.br>
2024-06-29 18:52:40 -0300

    Algumas mensagens criadas

--- EcoPowerTycoon/Assets/Scripts/ControlarMensagens.cs
+++ EcoPowerTycoon/Assets/Scripts/ControlarMensagens.cs
@@ -15,6 +15,21 @@ public class ControlarMensagens : MonoBehaviour
 
     public void MensagemDeDesbloqueioUsina()
     {
-        mensagemAtual.text = "Você Desbloqueou uma usina";
+        mensagemAtual.text = "Uau!! você desbloqueou uma usina, agora você produz 5GWZ/s. Estamos a todo vapor!";
+    }
+
+    public void MensagemDesbloqueioCentro()
+    {
+        mensagemAtual.text = "Você sabia que com pontos de pesquisa é possivel descobrir novos meios de produção e ainda melhora - los ?";
+    }
+    public void MensagemDesbloqueioMelhorias()
+    {
+        mensagemAtual.text = "Ecotlanta agradece por sempre buscar a melhor maneira de distribuir energia de forma ecologica.";
+    }
+
+    public void MensagemDeCompraDeUsinaEolica()
+    {
+        mensagemAtual.text = "Você sabia que a energia eólica é sustentavel porque ela ultiliza um recurso que é " +
+            "reutilizavel, o vento, mas que as turbinas proximas as casas atrapalham a qualidade de vida das pessoas?";
     }
 }
