1d8d784a0f73ef663eafecf82ac32b5e2551f5ab
LyviaBeatriz010 <lyvia.v@escolar.ifrn.edu.br>
2024-07-21 00:01:12 -0300

    Cutscene iniciada

---
+++ EcoPowerTycoon/Assets/Scripts/DigitarTexto.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+
+public class DigitarTexto : MonoBehaviour
+{
+    private TextMeshProUGUI componenteTexto;
+   
+    private AudioSource audioSource;
+    
+    private string mensagemOriginal;
+    
+    public bool imprimindo;
+    
+    public float tempoEntreLetras = 0.08f;
+
+    private void Awake()
+    {
+        TryGetComponent(out componenteTexto);
+        TryGetComponent(out audioSource);
+        mensagemOriginal = componenteTexto.text;
+        componenteTexto.text = "";
+    }
+
+    private void OnEnable()
+    {
+       ImprimirMensagem(mensagemOriginal); 
+    }
+
+    private void OnDisable()
+    {
+        componenteTexto.text = mensagemOriginal;
+        StopAllCoroutines();
+    }
+
+    public void ImprimirMensagem(string mensagem)
+    {
+        if (gameObject.activeInHierarchy)
+        {
+            if (imprimindo) return;
+            imprimindo = true;
+            StartCoroutine(LetraPorLetra(mensagem));
+           
+                
+        }
+    }
+
+    IEnumerator LetraPorLetra(string mensagem)
+    {
+        string msg = "";
+        foreach (var letra in mensagem)
+        {
+            msg += letra;
+            componenteTexto.text = msg;
+            audioSource.Play();
+            yield return new WaitForSeconds(tempoEntreLetras);
+        }
+        
+        imprimindo = false;
+        StopAllCoroutines();
+    }
+
+   
+}
