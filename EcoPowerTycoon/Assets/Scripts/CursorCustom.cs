ca4184701cef84fd6cb2bd14147b1c3152f3e701
LYVIA BEATRIZ VALENTIM EVANGELISTA <20211170150001@ifrn.local>
2024-08-07 10:21:36 -0300

    Cursor Vitual feito

--- EcoPowerTycoon/Assets/Scripts/CursorCustom.cs
+++ EcoPowerTycoon/Assets/Scripts/CursorCustom.cs
@@ -1,8 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CursorCustom : MonoBehaviour
 {
-     
+     public RectTransform pezinho;
+     public Camera maincam;
+
+     private void Update()
+     {
+          Vector3 mousePosition = Input.mousePosition;
+          Vector3 worldPosition = maincam.ScreenToWorldPoint(mousePosition);
+
+          worldPosition.z = 0;
+          pezinho.position = worldPosition;
+     }
 }
